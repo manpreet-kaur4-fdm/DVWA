69e966ba9ae4a654efb5de3d598650fa7f80c09b
manpreet-kaur4-fdm <137915644+manpreet-kaur4-fdm@users.noreply.github.com>
2023-06-29 09:55:29 -0400

    Update dvwaPage.js

--- dvwa/js/dvwaPage.js
+++ dvwa/js/dvwaPage.js
@@ -25,15 +25,15 @@ function validateGuestbookForm(thisform) {
 with (thisform) {
 
   // Guestbook form
-  if (validate_required(txtName,"Name can not be empty.")==false)
+  if (validate_required(txtName,"Subject can not be empty.")==false)
   {txtName.focus();return false;}
   
-  if (validate_required(mtxMessage,"Message can not be empty.")==false)
+  if (validate_required(mtxMessage,"Comment can not be empty.")==false)
   {mtxMessage.focus();return false;}
   
   }
 }
 
 function confirmClearGuestbook() {
-	return confirm("Are you sure you want to clear the guestbook?");
+	return confirm("Are you sure you want to clear the Feedbacks?");
 }
